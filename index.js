740e636176e255a8bd31d0f7b22c442755bd5dec
0xflotus <0xflotus@gmail.com>
2026-03-26 22:22:07 +0100

    perf: change implementation (#6)
    * perf: change implementation
    
    * chore: formatting
    
    * fix: rename object
    
    * chore: formatting

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-// prettier-ignore
-const TABLE = Array.of(
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x'
-  );
+const TABLE = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx";
+const LOOKUP = Object.fromEntries([...TABLE].map((c, i) => [c, i]));
+const BASE60_REGEX = /^[0-9A-Za-x]{7}$/;
 
 export const toJSDate = (base60) => {
-  if (!/^[0-9A-Za-x]{7}$/.test(base60)) {
-    throw new Error("It\'s not a valid base 60 string");
+  if (!BASE60_REGEX.test(base60)) {
+    throw new Error("It's not a valid base 60 string");
   }
 
-  const mem = base60.split("").map((char) => TABLE.indexOf(char));
-  return new Date(
-    Date.UTC(mem[0] * 60 + mem[1], mem[2], mem[3] + 1, mem[4], mem[5], mem[6]),
-  );
+  const m0 = LOOKUP[base60[0]];
+  const m1 = LOOKUP[base60[1]];
+  const m2 = LOOKUP[base60[2]];
+  const m3 = LOOKUP[base60[3]];
+  const m4 = LOOKUP[base60[4]];
+  const m5 = LOOKUP[base60[5]];
+  const m6 = LOOKUP[base60[6]];
+
+  return new Date(Date.UTC(m0 * 60 + m1, m2, m3 + 1, m4, m5, m6));
 };
 
-export const toBase60 = (datetime) => {
-  if (!(datetime instanceof Date)) {
-    throw new Error('"datetime" is not a valid Date object');
+export const toBase60 = (date) => {
+  if (!(date instanceof Date)) {
+    throw new Error('"date" is not a valid Date object');
   }
 
-  return [
-    Math.floor(datetime.getUTCFullYear() / 60),
-    datetime.getUTCFullYear() % 60,
-    datetime.getUTCMonth(),
-    datetime.getUTCDate() - 1,
-    datetime.getUTCHours(),
-    datetime.getUTCMinutes(),
-    datetime.getUTCSeconds(),
-  ]
-    .map((num) => TABLE[num])
-    .join("");
+  const year = date.getUTCFullYear();
+
+  return (
+    TABLE[Math.floor(year / 60)] +
+    TABLE[year % 60] +
+    TABLE[date.getUTCMonth()] +
+    TABLE[date.getUTCDate() - 1] +
+    TABLE[date.getUTCHours()] +
+    TABLE[date.getUTCMinutes()] +
+    TABLE[date.getUTCSeconds()]
+  );
 };
